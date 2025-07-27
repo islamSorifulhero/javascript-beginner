e6475b44493a7dac116d387148859a492979619c
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-27 11:05:19 +0600

    javascript module 1 done

--- first.js
+++ first.js
@@ -1,17 +1,72 @@
+// long variable
+var myCurrentHomeAddress = 'dhaka'; //camel case  : most useful 100%
+var my_current_home_address = 'dhaka'; //snak case
+var MyCurrentHomeAddress = 'dhaka'; //pascal case
+
+
 //number
 var age = 33;
-console.log(age);
-console.log(typeof age);
+// console.log(age);
+// console.log(typeof age);
 
 //string
 var name = 'soriful';
-console.log(name);
-console.log(typeof name);
+// console.log(name);
+// console.log(typeof name);
 
 //Boolean
 var isRich = true;
-console.log(isRich);
-console.log(typeof isRich);
+// console.log(isRich);
+// console.log(typeof isRich);
 var passed = false;
-console.log(passed);
-console.log(typeof passed);
+// console.log(passed);
+// console.log(typeof passed);
+
+//a = 5 (integer)//  //a = 5.55 (Float)//
+var orangePrice = 20;
+var chocolate = 0.5;
+var strawberry = '33.4';
+// console.log(typeof strawberry);
+var strawberry2 = parseInt('36.49584');
+var applePrice = parseFloat('33.4');
+// console.log(strawberry2);
+// console.log(typeof applePrice);
+var strawberry3 = 19.342;
+// console.log(strawberry3.toFixed(1));
+
+
+// advanced
+var price1 = 35;
+// price1 = price1 + 10; // short hand // price1 += 10;
+price1 += 10;
+// console.log(price1);
+
+
+// practice problem 1
+
+var orangePrice = 300;
+var applePrice = 400;
+var totalPrice = orangePrice + applePrice;
+// console.log(totalPrice);
+var haveMoney = 1000;
+var returnMoney = haveMoney - totalPrice;
+// console.log(returnMoney);
+
+
+//practice problem 2
+
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalMarks = mathematics + biology + chemistry + physics + bangla;
+// console.log(totalMarks);
+var avaresMarks = totalMarks / 5 ;
+// console.log(avaresMarks.toFixed(2));
+
+//practice problem 3;
+
+var number = 119;
+var remainder = number % 5;
+console.log(remainder);
