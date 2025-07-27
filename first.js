7544a4666908f75061c332e25274eee06be76dbf
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-27 07:29:05 +0600

    number string boolean practice done

---
+++ first.js
@@ -0,0 +1,17 @@
+//number
+var age = 33;
+console.log(age);
+console.log(typeof age);
+
+//string
+var name = 'soriful';
+console.log(name);
+console.log(typeof name);
+
+//Boolean
+var isRich = true;
+console.log(isRich);
+console.log(typeof isRich);
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
