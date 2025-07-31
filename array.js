3aa0ae7537f2f5062371761c22b3eb9537069a3e
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-31 23:58:36 +0600

    My array practice done

--- array.js
+++ array.js
@@ -8,7 +8,37 @@
 // num.push(88, 44, 55, 88, 44, 55);
 // console.log(num);
 
-let num = [3, 8, 7, 99, 76, 45, 11, 21];
-let updateNum = num.pop();
-console.log(num);
-console.log(updateNum);
+// let num = [3, 8, 7, 99, 76, 45, 11, 21];
+// let updateNum = num.pop();
+// console.log(num);
+// console.log(updateNum);
+
+// let num = [88, 44, 55, 66, 33]
+// num.pop();
+// console.log(num);
+
+// let num = [88, 44, 55, 66, 33]
+// num.pop();
+// num.pop();
+// num.pop();
+// console.log(num);
+
+// const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// console.log(num.indexOf(7));
+
+// const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// console.log(num.indexOf[7]);
+
+// const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// num [2] = 77;
+// console.log(num);
+
+
+// const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// console.log(num.includes(7));
+// console.log(num.includes(76));
+// console.log(num.includes(176));
+
+
+// const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// console.log(Array.isArray(num));
