7775fafd17e184e0003f87416ae7404750ddfbb7
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-07-31 23:09:06 +0600

    array practice Done

--- array.js
+++ array.js
@@ -1,2 +1,14 @@
-const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// console.log(num);
+
+// const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// console.log(num.length);
+
+// const num = [3, 8, 7, 99, 76, 45, 11, 21];
+// num.push(88, 44, 55, 88, 44, 55);
+// console.log(num);
+
+let num = [3, 8, 7, 99, 76, 45, 11, 21];
+let updateNum = num.pop();
 console.log(num);
+console.log(updateNum);
