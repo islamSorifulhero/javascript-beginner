cfc1f61253414fc60014d0fda7e4176827f28299
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-03 21:24:38 +0600

    problem solving done

---
+++ prbSolve.js
@@ -0,0 +1,10 @@
+// let totalRows = 20;
+// for (let i = 1; i <= 20; i++) {
+//     if(i % 3 === 0 || i % 5 === 0){
+//         console.log('Row ' + i + ' - ' + 'Premium Row');
+//     }
+//     else{
+//         console.log('Row ' + i + ' - ' + 'Normal Row');
+//     }
+// }
+
