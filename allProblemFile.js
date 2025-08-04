592db3ce4e31a69f391c3065962daff0f1ef497e
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-04 07:40:39 +0600

    my problem -04 complete

--- allProblemFile.js
+++ allProblemFile.js
@@ -30,3 +30,21 @@ for(var i = 1; i <= lastDay; i++){
         console.log(i + " - " + "rest");
     }
 }
+
+
+
+/** Problem 04 - (Delete / Store) */
+var fileName= 'docx.xpdf';
+//write your code here
+if(fileName[0] === '#'){
+    console.log("Store");
+}
+else if(fileName.endsWith('.pdf')){
+    console.log("Store");
+}
+else if(fileName.endsWith('.docx')){
+    console.log("Store");
+}
+else{
+    console.log("Delete");
+}
