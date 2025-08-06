4760e497acf5dc3d514f7d6fa42f989109ccdd66
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-06 23:12:25 +0600

    More About Array traversal and Operation

---
+++ bonasProblem.js
@@ -0,0 +1,54 @@
+// console.log("Today We will learning Function");
+
+// function hello() {
+//     console.log("I learn English. Because, I have a dream to be a fluently English spoken English.");
+// }
+// hello();
+
+// function moneybag() {
+//     var money = 4500;
+//     console.log('I have', money, 'taka');
+// }
+// moneybag();
+
+// function jogKorarMachin(input1, input2) {
+//     let sum = input1 + input2;
+//     console.log("result is a :", sum);
+// }
+// jogKorarMachin(10, 43);
+// jogKorarMachin(33, 67)
+
+// function sumNum (input1, input2){
+//     var sum = input1 + input2;
+//     console.log("sum num is :", sum);
+// }
+// let result = sumNum(10, 20);
+// console.log(result);
+
+
+// function sumNum(input1, input2) {
+//     var sum = input1 + input2;
+//     console.log("sum num is :", sum);
+//     return sum;
+// }
+// let result = sumNum(10, 20);
+// console.log(result);
+
+
+// practice 01 subtraction;
+
+
+
+
+
+
+// practice 02 multiple;
+
+
+
+
+
+
+
+// practice 03 division;
+
