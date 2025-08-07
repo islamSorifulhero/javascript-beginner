e2fefec0438ff90dda93e43e4690b553d8fea22f
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-08 01:35:56 +0600

    object array practice done

---
+++ More About Array/copy.js
@@ -0,0 +1,16 @@
+// const price = 25;
+// const comp_price = price;
+// console.log(price);
+// console.log(comp_price);
+
+// let price = 25;
+// const comp_price = price;
+// price = 32;
+// console.log(price);
+// console.log(comp_price);
+
+const prodect = [25, 25, 25];
+const comp_price = prodect;
+comp_price[0] = 15;
+console.log(prodect);
+console.log(comp_price);
---
+++ More About Array/object-array.js
@@ -0,0 +1,25 @@
+// const numbers = [1, 2, 4, 5, 6]
+// const players = [{}, {}, {}];
+
+
+// const employees = [
+//     {name: 'Ashik', designation: 'Conten Writer', salary: 29000},
+//     {name: 'Atik', designation: 'developer', salary: 29000},
+//     {name: 'Abdur Rahim', designation: 'Digital Marketer', salary: 21000},
+// ]
+// console.log(employees);
+
+
+// const employees = [
+//     {name: 'Ashik', designation: 'Conten Writer', salary: 29000},
+//     {name: 'Atik', designation: 'developer', salary: 29000},
+//     {name: 'Abdur Rahim', designation: 'Digital Marketer', salary: 21000},
+// ]
+// console.log(employees[0]);
+
+const employees = [
+    {name: 'Ashik', designation: 'Conten Writer', salary: 29000},
+    {name: 'Atik', designation: 'developer', salary: 29000},
+    {name: 'Abdur Rahim', designation: 'Digital Marketer', salary: 21000},
+]
+console.log(employees[0].salary);
