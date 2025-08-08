a50031f1c6b0f5dd9a6088bc78a02fdaa9cd62df
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-08 16:56:18 +0600

    My parameter practice done

---
+++ More About Array/parameter.js
@@ -0,0 +1,8 @@
+function vaatKhao(){
+    console.log('hand wash kore aso');
+    console.log('boso');
+    console.log('plate e khabar dhabar nao');
+    console.log('gopat gopat kore khaw');
+    console.log('onek beshi khaite hobe');
+}
+vaatKhao();
