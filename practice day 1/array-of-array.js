e5da41306053da3c3448e9d534e4f418b11a60eb
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-09 20:25:50 +0600

    my array of array practice complete

---
+++ practice day 1/array-of-array.js
@@ -0,0 +1,26 @@
+// let doubleArray = [
+//     ['kalam'],
+//     ['jamal'],
+//     ['tomal'],
+//     ['samad'],
+//     ['hablu']
+// ];{
+
+//     console.log(doubleArray[0] = "kakakamal");
+//     console.log(doubleArray);
+// }
+
+
+const numbers = [
+    [32,3,543,21,],
+    [53,13,743,213,],
+    [34,23,143,11,],
+    [39,33,343,221,]
+];{
+    const updateArray = numbers[0];
+    // console.log(updateArray[0]);
+    const change = updateArray[0] = 132;
+    console.log(change);
+    // console.log(updateArray[0][0]);
+    // console.log(numbers[0]);
+}
