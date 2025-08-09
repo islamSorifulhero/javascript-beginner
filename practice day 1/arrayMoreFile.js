a372ca8cdad411266b777316981661767c08109d
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-09 16:24:44 +0600

    My array more file practice done

---
+++ practice day 1/arrayMoreFile.js
@@ -0,0 +1,45 @@
+// const fruits = ['apple', 'Banana', 'anar', 'watermelo;n', 'davv', 'tall']; {
+//     console.log(fruits);
+// }
+
+
+// let fruits = [
+//     "banana",
+//     "apple",
+//     "orange",
+//     "watermelon",
+//     "jackfruits",
+//     "anar",
+//     "davv",
+//     "tall"
+// ];{
+//     console.log(fruits);
+//   }
+
+
+// const fruits = ['apple', 'Banana', 'anar', 'watermelo;n', 'davv', 'tall']; {
+//     for(let i = 0; i < fruits.length; i++){
+//         // console.log(fruits[i]);
+//         console.log(fruits[i]);
+//     }
+// }
+
+
+
+// let numbers = [33, 553, 23, 5, 43, 654, 77,];{
+//     // console.log(numbers);
+//     for(let i = 0; i < numbers.length; i++){
+//         console.log(numbers[i]);
+//     }
+// }
+
+//while
+
+var numbers = [33, 43, 53, 543, 134, 56]; {
+    let i = 0;
+    while (i < numbers.length) {
+        console.log(numbers[i]);
+        i++;
+    }
+}
+
