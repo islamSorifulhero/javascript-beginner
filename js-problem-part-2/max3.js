6e4eaeb01f7c3ef5a6e57f8777279e12830ac3d3
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-11 16:58:47 +0600

    my tallest num practice done

---
+++ js-problem-part-2/max3.js
@@ -0,0 +1,28 @@
+// const jim = 32;
+// const kim = 194;
+// const tim = 152;
+// if(jim > kim && jim > tim){
+//     console.log("jim is biggest man");
+// }else if(kim > jim && kim > tim){
+//     console.log("kim is big man kopa kopa");
+// }else{
+//     console.log("tim is topasamsu");
+// }
+
+
+
+
+function maxNumbers(num1, num2, num3) {
+    // if (num1 > num2 && num1 > num3) {
+    //     console.log("num1 is big");
+    // } else if (num2 > num1 && num2 > num3) {
+    //     console.log("num 2 is bigger");
+    // } else {
+    //     console.log("num 3 is okkkk");
+    // }
+}
+// const result = maxNumbers(145, 164, 75);
+// console.log(result);
+
+const max = Math.max(33, 45, 54, 5, 24, 34, 2, 7, 8)
+console.log(max);
---
+++ js-problem-part-2/tallest.js
@@ -0,0 +1,17 @@
+// const heights = [64, 65, 224, 87, 86, 144];
+
+// function getMax (numbers){
+//     let max = numbers[0]
+//     for(let num of numbers){
+//         if(num > max){
+//             max = num;
+//         }
+//     }
+//     return max;
+// }
+// const max = getMax(heights);
+// console.log(max);
+
+//homework: min number 
+
+//start - 12.08.25
