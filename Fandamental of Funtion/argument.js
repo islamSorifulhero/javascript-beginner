b7888883d499c4f1b4473c76032d72742817a4a3
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-12 23:33:41 +0600

    argument function practice done

---
+++ Fandamental of Funtion/argument.js
@@ -0,0 +1,14 @@
+// function numbers(num){
+//     const multiple = num * 2;
+//     console.log(multiple);
+// }
+// const age = 9;
+// numbers(9);
+
+function sum(x, y) {
+    const totalSum = x + y;
+    console.log(totalSum);
+}
+const mathMark = 82;
+const englishMark = 78;
+sum(mathMark, englishMark);
