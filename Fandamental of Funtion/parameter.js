38a4416cb66338bd590c8b9a4886e8156f58ead2
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-12 23:14:29 +0600

    parameter function practice done

---
+++ Fandamental of Funtion/parameter.js
@@ -0,0 +1,29 @@
+// function square(number){
+//     console.log(number);
+// }
+// square('bangladesh is big country, and there are lot of money');
+
+
+
+// function numbers(num1, num2){
+//     const total = num1 +num2;
+//     console.log('Tolal num is :', total);
+// }
+// numbers(40, 60);
+// numbers(25, 65);
+
+
+// function sum(a, b, c, d, e, f, g) {
+//     const totalSum = a + b + c + d + e + f + g;
+//     console.log("Total Sum Numbers is: ", totalSum);
+// }
+// sum(21, 12, 34, 53, 65, 78, 76);
+
+function borgo(number1) {
+    const borgoNumIs = number1 * number1;
+    console.log("Two Numbers multiple value is: ", borgoNumIs);
+}
+borgo(5);
+borgo(25);
+borgo(9);
+borgo(6);
