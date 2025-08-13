31c2c1366be444fdd267e1f51dc76dd2ddb32e9f
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-13 19:26:15 +0600

    math practice done

---
+++ js-problems-part-1/math.js
@@ -0,0 +1,18 @@
+// const min = [43, 2, 654, 56, 245, 86];
+// console.log(min);
+
+
+// const max = Math.max(23, 434, 645, 2137, 6545, 34);
+// console.log(max);
+
+// const min = Math.min(534, 234, 654, 7, 476, 43, 2);
+// console.log(min);
+
+// console.log(Math.PI);
+// console.log(Math.abs(5 - 20));
+// console.log(Math.round(3.453));
+// console.log(Math.round(3.553));
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
+
+
