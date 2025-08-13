885c535588c3e9d9ae4ae4631dadc0782a061e87
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-13 07:15:20 +0600

    return again recap practice done

---
+++ Fandamental of Funtion/recap.js
@@ -0,0 +1,17 @@
+// function sum(a, b) {
+//     const total = a + b;
+//     return total;
+// }
+// const cost = sum(5, 8);
+// console.log(cost);
+
+
+function doMath(a, b) {
+    const sum = a + b;
+    const diff = a - b;
+    const multiple = a * b;
+    const result = multiple / 2;
+    return result
+}
+const result = doMath(5, 6);
+console.log(result);
--- Fandamental of Funtion/return.js
+++ Fandamental of Funtion/return.js
@@ -1,5 +1,6 @@
 function tentimes(num){
     const total = num * 10;
     console.log(total);
+    return total;
 }
 tentimes(5);
