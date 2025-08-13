aa1d30f1d38434ed3b9cf971616b92e4441226e1
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-13 22:51:34 +0600

    tallest practice done

--- js-problem-part-2/tallest.js
+++ js-problem-part-2/tallest.js
@@ -14,4 +14,30 @@
 
 //homework: min number 
 
-//start - 12.08.25
+
+// const heights = [65, 66, 68, 72, 78, 60, 65, 66];
+// function lowesNum (numbers){
+//     let firstNum = numbers[0];
+//     for(let number of numbers){
+//         if(number > firstNum){
+//             firstNum = number;
+//         }
+//     }
+//     return firstNum;
+// }
+// const result = lowesNum(heights)
+// console.log(result);
+
+
+const heights = [65, 66, 68, 72, 78, 36, 165, 166];
+function getMin (numbers){
+    let min = numbers[0];
+    for(const numb of numbers){
+        if(numb < min){
+            min = numb;
+        }
+    }
+    return min;
+}
+const min = getMin(heights);
+console.log(min);
