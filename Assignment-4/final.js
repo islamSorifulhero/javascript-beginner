725297be51e96181f827cce4f755cc15777d1c68
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-15 07:39:17 +0600

    final tasks submit done

--- Assignment-4/final.js
+++ Assignment-4/final.js
@@ -57,4 +57,30 @@ function isSame(arr1, arr2) {
 }
 
 
-
+function resultReport(marks) {
+    // You have to write your code here
+    if (!Array.isArray(marks)) {
+        return "Invalid";
+    }
+    if (marks.length === 0) {
+        return { finalScore: 0, pass: 0, fail: 0 };
+    }
+    let total = 0;
+    let passCount = 0;
+    let failCount = 0;
+    for (let i = 0; i < marks.length; i++) {
+        total += marks[i];
+        if (marks[i] >= 40) {
+            passCount++;
+        }
+        else {
+            failCount++;
+        }
+    }
+    let avgNum = Math.round(total / marks.length);
+    return {
+        finalScore: avgNum,
+        pass: passCount,
+        fail: failCount
+    };
+}
