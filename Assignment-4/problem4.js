b8b81efc7fcd763b81b1ebfc0651e76858349c83
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-15 06:33:41 +0600

    problem 4 done

---
+++ Assignment-4/problem4.js
@@ -0,0 +1,19 @@
+let arr1 = [1, 4, 5];
+let arr2 = [1, 4, 5];
+function isSame(arr1, arr2) {
+    // You have to write your code here
+    if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+        return "Invalid";
+    }
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+const result = isSame(arr1, arr2);
+console.log(result);
