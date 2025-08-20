ffd17f883f249a9bfbf562ba8fca04a3196d5e2f
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-20 23:55:04 +0600

    function related tasks practice done

--- Fandamental of Funtion/function-releted-tasks.js
+++ Fandamental of Funtion/function-releted-tasks.js
@@ -1,6 +1,17 @@
-function fourNum(num1, num2, num3, num4) {
-    const multiply = num1* num2 * num3* num4;
-    console.log(multiply);
-    return multiply;
+//tasks 01
+
+// function fourNum(num1, num2, num3, num4) {
+//     const multiply = num1* num2 * num3* num4;
+//     console.log(multiply);
+//     return multiply;
+// }
+// fourNum(2, 3, 4, 5);
+
+// or simple type
+
+function multiplyFour(a, b, c, d) {
+    return a * b * c * d;
 }
-fourNum(2, 3, 4, 5);
+console.log(multiplyFour(2, 3, 4, 5));
+const result = multiplyFour(2, 3, 4, 5)
+console.log(result);
