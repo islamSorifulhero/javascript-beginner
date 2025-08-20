ced55ef60475e3aab6b10279a880b23876f978f5
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-20 22:41:33 +0600

    conceptual sessions - 2

---
+++ Conceptual Sessions/session1.js
@@ -0,0 +1,8 @@
+// const numbers = [30, 20, 10, 100, 200, 50];
+// numbers.sort(function (a, b) {
+//     return a - b;
+// })
+// console.log(numbers);
+
+
+part:6 - 8.00 minute
---
+++ Conceptual Sessions/session2.js
@@ -0,0 +1,28 @@
+// function ATM (){
+//     let amount = 4000;
+//     console.log("Welcome to ph Bank LTd");
+//     console.log("Your Amount " + amount + " Taka");
+// }
+// ATM();
+
+
+// function subjects (math){
+    // const exam = math * 2;
+    // console.log(exam);
+//     if(math > 33){
+//         console.log("you are passed");
+//     }
+//     else{
+//         console.log("you are fail");
+//     }
+// }
+// subjects(32);
+
+function square(input){
+    let squareNum = input * input;
+    console.log(squareNum);
+    return squareNum
+    console.log("code stoopedd");
+}
+let result = square(49);
+console.log(result);
