6bcd1d0f2fc6584eedc5b9064630359114ddb646
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-21 19:49:00 +0600

    function related tasks complete

--- Fandamental of Funtion/function-releted-tasks.js
+++ Fandamental of Funtion/function-releted-tasks.js
@@ -20,14 +20,53 @@
 
 // tasks - 02
 
-function oddEven (input){
-    if(input % 2 === 1){
-        // input * 2;
-        return input * 2;
-    }
-    else{
-        return input % 2;
+// function oddEven(input) {
+//     if (input % 2 === 1) {
+//         return input * 2;
+//     }
+//     else {
+//         return input / 2;
+//     }
+// }
+// const result = oddEven(90);
+// console.log(result);
+
+
+//tasks - 03
+
+// myself practice
+
+// function make_avg(avg) {
+//     let sum = 0;
+//     for (let i = 0; i < avg.length; i++) {
+//         sum += avg[i];
+//     }
+//     let avgNum = sum;
+//     let avgtotal = avgNum / 12;
+//     return avgtotal;
+// }
+// const result = make_avg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+// console.log(result);
+
+
+// origin answer
+
+// function make_avg(array, number) {
+//     let sum = 0;
+//     for(let i = 0; i < number; i++){
+//         sum += array[i];
+//     }
+//     return sum / number;
+// }
+// let arrayNum = [2, 3, 4, 5, 6, 40];
+// let avgNum = make_avg(arrayNum, arrayNum.length);
+// console.log(avgNum);
+
+
+function count_zero(binary){
+    if(binary.length === 0){
+        return binary.length;
     }
 }
-const result = oddEven(22);
+const result = count_zero("1100111101001000101");
 console.log(result);
