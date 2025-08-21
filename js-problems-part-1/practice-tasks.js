24c0d6bfb0804f99a4c943f63fa7c94af9d20d5c
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-21 23:56:22 +0600

    js problem tasks practice done

---
+++ js-problems-part-1/practice-tasks.js
@@ -0,0 +1,56 @@
+// tasks - 01
+
+// function celToFar(celsius) {
+//     return ((celsius * 9 / 5) + 32);
+//     // return celsius * 9/5 + 32;
+// }
+// const farhenheitResult = celToFar(0);
+// console.log(farhenheitResult);
+
+
+// tasks - 02
+
+
+// function repeated(array, find) {
+//     let single = 0;
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i] === find){
+//             single++;
+//         }
+//     }
+//     return single;
+// }
+
+// // let numbers = [5, 6, 11, 12, 98, 5];
+// // console.log(repeated(numbers, 5));
+
+// let numbers = [5, 6, 11, 12, 98, 5];
+// console.log(repeated(numbers, 25));
+
+
+
+// tasks - 03
+
+
+// function vowelsCount(vowels) {
+//     let count = 0;
+//     let vowelsCount = 'aeiouAEIOU';
+//     for (let i = 0; i < vowels.length; i++) {
+//         if (vowelsCount.includes(vowels[i])) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// // const result = vowelsCount("I am a student. I have a some problems");
+// // console.log(result);
+
+// console.log(vowelsCount("hello world"));
+
+
+
+// tasks - 04
+
+
+
+
