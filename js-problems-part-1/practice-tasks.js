6cc7e17889ed63699590dbd54a9f38cfcaa05e7a
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-22 12:00:25 +0600

    problems prat-1 tasks done

--- js-problems-part-1/practice-tasks.js
+++ js-problems-part-1/practice-tasks.js
@@ -70,15 +70,38 @@
 
 // or 
 
-function longestWord(string){
-    let str = string.split(' ');
-    let longest = " ";
-    for(let i = 0; i < str.length; i++){
-        if(str[i].length > longest.length){
-            longest = str[i]
-        }
-    }
-    return longest;
+// function longestWord(string){
+//     let str = string.split(' ');
+//     let longest = " ";
+//     for(let i = 0; i < str.length; i++){
+//         if(str[i].length > longest.length){
+//             longest = str[i]
+//         }
+//     }
+//     return longest;
+// }
+// const result = longestWord("I am learning Programming to become a programmer")
+// console.log(result);
+
+
+// tasks 05
+
+// function randomNum() {
+//     const min = 10;
+//     const max = 20;
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// const result = randomNum(10, 20);
+// console.log(result);
+
+// or
+
+// function randomBetween10and20() {
+//     return Math.floor(Math.random() * (20 - 10 + 1)) + 10;
+// }
+// console.log(randomBetween10and20());
+
+function random() {
+    return Math.floor(Math.random() * (20 - 10 + 1) + 10);
 }
-const result = longestWord("I am learning Programming to become a programmer")
-console.log(result);
+console.log(random());
